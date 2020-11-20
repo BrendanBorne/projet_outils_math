2a8e25dd44752fec4b81f654b5b888e0e7f8d114
brendanborne <brendan.borne@hotmail.fr>
2020-11-20 14:12:52 +0100

    Update traitement_fichier.m

--- traitement_fichier.m
+++ traitement_fichier.m
@@ -1,3 +1,37 @@
 clear all;
 close all;
 
+#Importation des données
+temp=load("Temp2011.txt");
+tab=[temp(:,2)];
+
+#Traitement des données
+tMoy=mean(tab);
+amp=max(tab)-min(tab);
+
+#Tableau t pour les données importées
+t=[];
+temp=[];
+for i=1:2:365
+  t=[t;i]; 
+endfor
+
+#Simulation de la température
+tsim=[];
+for i=1:365
+  tsim=[tsim;i];
+  calcul = tMoy + (amp/2 * sin(((2*pi)/365)*i+89.5));
+  temp=[temp;calcul];
+endfor
+
+#Aperçu des données
+figure(1);
+hold on;
+plot(t,tab);
+plot(tsim,temp);
+xlabel("Jours");
+ylabel("Temperature");
+legend("Valeurs réelles","Simulation");
+plot(t,tMoy, 'k');
+text(370, tMoy, "Moyenne");
+grid;
