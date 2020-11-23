bc0b6c922b906add9642004d891dd70181d4ff5f
brendanborne <brendan.borne@hotmail.fr>
2020-11-23 13:40:39 +0100

    Update bucherons.m

--- bucherons.m
+++ bucherons.m
@@ -1,7 +1,7 @@
 clear all;
 close all;
 
-#Importation des donnï¿½es
+#Importation des données
 buch=load("Bucherons.txt");
 n=buch(:,2);
 j=buch(:,1);
@@ -10,5 +10,6 @@ j=buch(:,1);
 figure(1);
 plot(j,n);
 title("Evolution du nombre de bucherons au cours du temps");
+xlabel("Jours");
 ylabel("Bucherons");
 grid;
