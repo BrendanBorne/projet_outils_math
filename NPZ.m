dc1c05ac292917556aefa5fab1652c22664be0bf
TheoFABIEN <fabien.theo@gmail.com>
2020-11-23 11:34:46 +0100

    Update NPZ.m

--- NPZ.m
+++ NPZ.m
@@ -8,7 +8,11 @@ global e
 global gamma
 global fIo
 
-Flux_nutriment = -((Vm*N)/(ks + N))*fIo*P + (1-gamma)*Z*Rm*(1-exp(-lambda*P)) + e*P + g*Z;
+global dt  %pas de temps, pour les apports habdomadaires des bucherons
+
+%A LIRE : je n'ai ajout‚ que l'utilisation des lieux d'aisance pas un seul bucheron, mais il y a un nombre variable de bucherons
+
+Flux_nutriment = -((Vm*N)/(ks + N))*fIo*P + (1-gamma)*Z*Rm*(1-exp(-lambda*P)) + e*P + g*Z + (0.00052/7)*dt;
 
 Flux_phyton = ((Vm*N)/(ks + N))*fIo*P - Z*Rm*(1-exp(-lambda*P)) - e*P;
 
