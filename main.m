2101aa589566c857dcaa38f5ca0d82e805211ac6
TheoFABIEN <fabien.theo@gmail.com>
2020-12-04 20:23:05 +0100

    Update main.m

--- main.m
+++ main.m
@@ -5,7 +5,7 @@ clear all;
 
 #VARIABLES
 
-#globales NPZ
+#Globales fonction NPZ
 global Vm
 global ks
 global Rm
@@ -15,7 +15,7 @@ global e
 global gamma
 global fIo
 
-#globales croissance
+#Globales fonction croissance
 global cT
 global Kref
 global Linf
@@ -24,13 +24,12 @@ global AmpT
 global Ta
 global Tref
 
+#Relatives aux bucherons
 TAB_bucherons = load('Bucherons.txt');          %tableau des bucherons
-global bucherons = 0;          %nombre de bucherons
+global bucherons = 0;                           %nombre de bucherons
 debut_bucherons = input("Jour d'arrivee des premiers bucherons ? ");
-%jours_bucherons = TAB_bucherons(:,1);   pas forc‚ment utile
-ligne = 0;  %pas forc‚ment utile non plus
 
-
+#Autres
 Vm = 1;                                         %en jour^-1, taux max d'assimilation d'azote par le phyton
 ks = 1;                                         %en micromole, cstte de demi-saturation de l'azote par le phyton
 Rm = 1;                                         %en jour^-1, taux max de broutage du phyton par les métynnis
@@ -41,7 +40,7 @@ gamma = 0.7;                                    %proportion d'azote assimile par
 fIo = 0.25;                                     %fonction de l'intensite de la lumiere 
 
 Tmax = 365*9;                                   %temps maximal de la simulation
-global dt = 0.5;                                %pas de temps
+dt = 0.5;                                       %pas de temps
 Time = [0];                                     %tableau qui stocke le temps
 
 N = 4;                                          %effectifs initiaux nutriments
@@ -75,12 +74,9 @@ for t = dt:dt:Tmax
   Effectifs_nutriment = [Effectifs_nutriment N];
   Effectifs_phyton = [Effectifs_phyton P];
   Time = [Time t];
-  
-  
 endfor
 
-#REPRESENTATION GRAPHIQUE
-
+#REPRESENTATION
 figure(1);
 hold on;
 plot(Time, Effectifs_mety, 'r');
@@ -94,23 +90,23 @@ title('Evolution des effectifs de methynnis, nutriments et phyton en fonction du
 # SIMULATION DE LA CROISSANCE DU MARSUPILAMI
 
 #VARIABLES POUR K
-Tmoy = 273+31.995;                               %température moyenne notée en kelvin
+Tmoy = 273+31.995;                       %température moyenne notée en kelvin
 temperature = Tmoy;
-AmpT = 10.366;                                  %amplitude thermique
+AmpT = 10.366;                           %amplitude thermique
 Tref = 310;
-Kref = 0.031/30;                             %on divise par 30 pour passer de mois en jours
-Ta = 9500;                                   %température d'Arhenius
+Kref = 0.031/30;                         %on divise par 30 pour passer de mois en jours
+Ta = 9500;                               %température d'Arhenius
 
 
 #AUTRES VARIABLES
-Linf = 150;                                  %cm
-L = 7;                                       %longueur initiale                             
-Xk = 0.14;                                   %valeur de demi saturation, en micromoles d'azote par litre
+Linf = 150;                              %cm
+L = 7;                                   %longueur initiale                             
+Xk = 0.14;                               %valeur de demi saturation, en micromoles d'azote par litre
 x = 0;
 Met = 0;
 
 #TABLEAUX
-Lt = [L];                                    %vecteur qui stocke les longueurs                             
+Lt = [L];                                %vecteur qui stocke les longueurs                             
 temp = [temperature];
 
 #CALCUL
@@ -119,15 +115,17 @@ for t = dt:dt:Tmax
   x = x+1;                                       
   [temperature, cT] = Fonction_var_temp(Tmoy, t);  
   Met = Effectifs_mety(1,x);
-  f = Met/(Xk + Met);                                          %reponse fonctionnelle
+  f = Met/(Xk + Met);    %reponse fonctionnelle
+  
   K1 = Fonction_var_taille(L);
   K2 = Fonction_var_taille(L + 0.5*dt*K1);
   K3 = Fonction_var_taille(L + 0.5*dt*K2);
   K4 = Fonction_var_taille(L + dt*K3);
+  
   L = L + dt*(K1/6 + K2/3 + K3/3 + K4/6);
+  
   Lt = [Lt L];
   temp = [temp temperature];
-  
 endfor
 
 #REPRESENTATION
